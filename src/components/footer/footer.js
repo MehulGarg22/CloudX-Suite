d21b19291805ad32a5b87d139e080087d5ec2286
MEHUL GARG <mehul.garg104@gmail.com>
2025-03-24 00:00:32 +0530

    Better prompt messages with additional use cases added

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import react from "react";
 export default function Footer(){
     return (
         <div style={{textAlign:'center'}}>
-            Copyright ©{(new Date().getFullYear())}- Developed by Mehul Garg
+            Copyright ©{(new Date().getFullYear())}- Developed by <a target="_blank" style={{color:'black', cursor:'pointer'}} href="https://mehulgarg.netlify.app/" >Mehul Garg</a> 
         </div>
     )
 }
