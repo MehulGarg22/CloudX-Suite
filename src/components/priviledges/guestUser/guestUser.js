798d4d71281d1690d1eca3220380e66cfec8089f
MEHUL GARG <mehul.garg104@gmail.com>
2025-04-03 23:14:21 +0530

    1. Removed text error from Guest table

--- src/components/priviledges/guestUser/guestUser.js
+++ src/components/priviledges/guestUser/guestUser.js
@@ -32,20 +32,20 @@ export default function GuestUser(){
             title: 'Joining Fee',
             dataIndex: 'joiningfee',
             key: 'joiningfee',
-            sorter: (a: Item, b: Item) => Number(a.joiningfee) - Number(b.joiningfee),
+            sorter: (a, b) => Number(a.joiningfee) - Number(b.joiningfee),
         },
         {
             title: 'Annual Fee',
             dataIndex: 'annualfee',
             key: 'annualfee',
-            sorter: (a: Item, b: Item) => Number(a.annualfee) - Number(b.annualfee),
+            sorter: (a, b) => Number(a.annualfee) - Number(b.annualfee),
         },
 
         {
             title: 'Zomato',
             dataIndex: 'zomato',
             key: 'zomato',
-            sorter: (a: Item, b: Item) => {
+            sorter: (a, b) => {
                 const zomatoA = a.zomato ? parseFloat(a.zomato.replace('%', '')) : 0;
                 const zomatoB = b.zomato ? parseFloat(b.zomato.replace('%', '')) : 0;
                 return zomatoA - zomatoB;
@@ -55,7 +55,7 @@ export default function GuestUser(){
             title: 'Swiggy',
             dataIndex: 'swiggy',
             key: 'swiggy',
-            sorter: (a: Item, b: Item) => {
+            sorter: (a, b) => {
                 const swiggyA = a.swiggy ? parseFloat(a.swiggy.replace('%', '')) : 0;
                 const swiggyB = b.swiggy ? parseFloat(b.swiggy.replace('%', '')) : 0;
                 return swiggyA - swiggyB;
@@ -65,7 +65,7 @@ export default function GuestUser(){
             title: 'Big Basket',
             dataIndex: 'bigbasket',
             key: 'bigbasket',
-            sorter: (a: Item, b: Item) => {
+            sorter: (a, b) => {
                 const bigbasketA = a.bigbasket ? parseFloat(a.bigbasket.replace('%', '')) : 0;
                 const bigbasketB = b.bigbasket ? parseFloat(b.bigbasket.replace('%', '')) : 0;
                 return bigbasketA - bigbasketB;
@@ -79,7 +79,7 @@ export default function GuestUser(){
         {
           title: 'Rewards',
           key: 'features',
-          render: (text: any, record: Item) => (
+          render: (text, record) => (
             <Table
               columns={[
                 {  dataIndex: 'featureName', key: 'featureName' },
